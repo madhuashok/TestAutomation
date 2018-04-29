e6aa4301438e8cf09bdd5bd714c07787bb2b086a
madhuashok2011@gmail.com <madhuashok2011@gmail.com>
2018-04-29 21:32:56 +0100

    Second Commit

--- Test/src/test/java/Test.java
+++ Test/src/test/java/Test.java
@@ -3,4 +3,8 @@ public class Test {
     public static void main(String args[]){
         System.out.println("First commmit");
     }
+
+    public void add(Integer j){
+        System.out.println("I am in add method");
+    }
 }
